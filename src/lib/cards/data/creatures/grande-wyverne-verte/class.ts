1a96f36d0505a8ae7620c36fdaa318a166d2c9fd
Algolbarth <paul.poirier@live.fr>
2026-05-05 15:48:14 +0200

    feat: add "Grande wyverne verte" card

---
+++ src/lib/cards/data/creatures/grande-wyverne-verte/class.ts
@@ -0,0 +1,34 @@
+import type { System } from '$lib/system/class';
+import type { Card } from '$lib/cards/class/class';
+import { Creature } from '$lib/cards/class/creature';
+
+export class GrandeWyverneVerte extends Creature {
+    name = "Grande wyverne verte";
+
+    constructor(system: System) {
+        super(system);
+
+        this.level = 10;
+        this.init([["Or", 90], ["Nature", 90]]);
+
+        this.initFamily(["Reptile", "Wyverne"]);
+
+        this.stat("Constitution").init(100);
+        this.stat("Force").init(50);
+
+        this.addText(`Quand une carte alliée d'élément Nature est posée : Si sur la pile : Réduit de 20 son coût.`);
+        this.addText(`Quand se prépare sur le terrain : Augmente de 15 sa constitution.`);
+    };
+
+    otherPoseEffect = (card: Card) => {
+        if (this.isArea("Pile") && this.isAlly(card) && card.isElement("Nature")) {
+            this.costReduce(20);
+        }
+    };
+
+    startPhaseEffect = () => {
+        if (this.isArea("Terrain")) {
+            this.stat("Constitution").increase(15);
+        }
+    };
+};
