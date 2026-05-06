cb6b6dec502dd8b3b8eef9db628188748ad2d486
Algolbarth <paul.poirier@live.fr>
2026-05-06 10:28:04 +0200

    feat: add "Ancienne wyverne dorée" card

---
+++ src/lib/cards/data/creatures/ancienne-wyverne-doree/class.ts
@@ -0,0 +1,33 @@
+import type { System } from '$lib/system/class';
+import type { Card } from '$lib/cards/class/class';
+import { Creature } from '$lib/cards/class/creature';
+
+export class AncienneWyverneDoree extends Creature {
+    name = "Ancienne wyverne dorée";
+
+    constructor(system: System) {
+        super(system);
+
+        this.level = 20;
+        this.init([["Or", 450]]);
+
+        this.initFamily(["Reptile", "Wyverne"]);
+
+        this.stat("Constitution").init(200);
+        this.stat("Force").init(200);
+
+        this.addText(`Quand une carte alliée d'élément Neutre est posée : Si sur la pile : Réduit de 50 son coût.`);
+        this.addText(`Quand une carte alliée d'élément Neutre est posée : Si sur le terrain : Augmente de 1 votre production d'or.`);
+    };
+
+    otherPoseEffect = (card: Card) => {
+        if (this.isAlly(card) && card.isElement("Neutre")) {
+            if (this.isArea("Pile")) {
+                this.costReduce(50);
+            }
+            else if (this.isArea("Terrain")) {
+                this.owner().ressource("Or").increase(1);
+            }
+        }
+    };
+};
---
+++ src/lib/cards/data/creatures/grande-wyverne-doree/class.ts
@@ -0,0 +1,34 @@
+import type { System } from '$lib/system/class';
+import type { Card } from '$lib/cards/class/class';
+import { Creature } from '$lib/cards/class/creature';
+
+export class GrandeWyverneDoree extends Creature {
+    name = "Grande wyverne dorée";
+
+    constructor(system: System) {
+        super(system);
+
+        this.level = 10;
+        this.init([["Or", 180]]);
+
+        this.initFamily(["Reptile", "Wyverne"]);
+
+        this.stat("Constitution").init(80);
+        this.stat("Force").init(80);
+
+        this.addText(`Quand une carte alliée d'élément Neutre est posée : Si sur la pile : Réduit de 20 son coût.`);
+        this.addText(`Quand se prépare sur le terrain : Augmente de 2 votre production d'or.`);
+    };
+
+    otherPoseEffect = (card: Card) => {
+        if (this.isArea("Pile") && this.isAlly(card) && card.isElement("Neutre")) {
+            this.costReduce(20);
+        }
+    };
+
+    startPhaseEffect = () => {
+        if (this.isArea("Terrain")) {
+            this.owner().ressource("Or").increase(2);
+        }
+    };
+};
